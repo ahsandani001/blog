071c6765068209c156682eab75476796b81b5663
Ahsan Danish <ahsan.danish7427@gmail.com>
2025-06-11 12:16:47 +0500

    integrated event bus to posts and comments

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {randomBytes} = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require("cors")
+const axios = require("axios")
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const {content} = req.body;
   
@@ -21,8 +22,19 @@ app.post('/posts/:id/comments', (req, res) => {
   comments.push({id: commentId, content});
   commentsByPostId[req.params.id] = comments;
 
+  await axios.post("http:localhost:4005/events", {
+    type: "CommentCreated",
+    data: {id: commentId, content, postId: req.params.id}
+  });
+
   res.status(201).send(comments);
 });
+
+app.post('/events', (req, res) => {
+  console.log("received events ==================> ", req.body.type)
+
+  res.send({})
+})
 
 app.listen(PORT, () => {
   console.log(`Comments server running on port ${PORT}`);
