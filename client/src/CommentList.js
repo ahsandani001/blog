e44d9c0a9cdb684fc9120cbbb5b2cd95023fd42e
Ahsan Danish <ahsan.danish7427@gmail.com>
2025-06-11 13:23:06 +0500

    integrated query service to make project independent

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,18 +1,6 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+import React from "react";
 
-const CommentList = ({postId}) => {
-    const [comments, setComment] = useState([]);
-
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComment(res.data);
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, []);
-
+const CommentList = ({comments}) => {
     const renderedComments = comments.map(comment => {
         return <li key={comment.id}>{comment.content}</li>
     })
