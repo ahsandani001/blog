43c929625e0d84a8df4dfa8ba75c9182e5b2c432
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-06-12 21:00:15 +0500

    Added nodePort and ClusterIp services and wired them up (Release)
    Added clusterIP and nodePort services and wired them up

--- query/index.js
+++ query/index.js
@@ -42,6 +42,7 @@ app.get('/posts', (req, res) => {
 
 app.post('/events', (req, res) => {
     const {type, data} = req.body;
+    console.log("received events ==================> ", type)
 
     handleEvents(type, data);
 
@@ -51,7 +52,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
     console.log('Listening on 4002');
 
-    const res = await axios.get('http://localhost:4005/events')
+    const res = await axios.get('http://event-bus-srv:4005/events')
 
     for(let event of res.data){
         console.log('Processing event: ', event.type);
